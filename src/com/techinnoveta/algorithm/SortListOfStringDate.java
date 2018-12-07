98a1db9e3c88d3b56a1405771d03a18b5008f283
Thanushan Kanagarajah <ccev1681@nus.edu.sg>
2018-12-07 17:28:32 +0800

    Committed

--- src/com/techinnoveta/algorithm/SortListOfStringDate.java
+++ src/com/techinnoveta/algorithm/SortListOfStringDate.java
@@ -1,4 +1,5 @@
 package com.techinnoveta.algorithm;
+
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -14,22 +15,22 @@ public class SortListOfStringDate {
 		dateList.add("02/12/2001");
 		dateList.add("12/09/1999");
 		dateList.add("13/11/2016");
-		
-		Comparator<String> comparator = new Comparator<String>(){
+
+		Comparator<String> comparator = new Comparator<String>() {
 			@Override
 			public int compare(String o1, String o2) {
-				SimpleDateFormat format=new SimpleDateFormat("dd/MM/yyyy");
+				SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
 				try {
 					Date date1 = format.parse(o1);
 					Date date2 = format.parse(o2);
 					return date1.compareTo(date2);
 				} catch (ParseException e) {
 					return 0;
-				}				
+				}
 			}
-			
+
 		};
-		
+
 		Collections.sort(dateList, comparator);
 		for (String string : dateList) {
 			System.out.println(string);
---
+++ src/com/techinnoveta/thread/SubWorker.java
@@ -0,0 +1,12 @@
+package com.techinnoveta.thread;
+
+public class SubWorker implements Runnable{
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+}
