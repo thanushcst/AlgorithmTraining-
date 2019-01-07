2eaefb519ee5346b97556217641e1cf1db3e5e7e
Thanushan Kanagarajah <ccev1681@nus.edu.sg>
2019-01-07 19:55:42 +0800

    Committed

--- src/com/techinnoveta/java8/functional/Draw.java
+++ src/com/techinnoveta/java8/functional/Draw.java
@@ -1,14 +1,31 @@
 package com.techinnoveta.java8.functional;
 
 @FunctionalInterface
-public interface Draw {
+interface DrawInterface {
 	void draw();
-	default void print(){
+
+	default void print() {
 		System.out.println("PRINT");
 	}
-	
-	static void click(){
+
+	static void click() {
 		System.out.println("CLICK");
 	}
 
+	default void click_test() {
+		System.out.println("CLICK");
+	}
+}
+
+public class Draw {
+	public static void main(String[] args) {
+		DrawInterface drawInterface = new DrawInterface() {
+			
+			@Override
+			public void draw() {
+				// TODO Auto-generated method stub
+				
+			}
+		};
+	}
 }
---
+++ src/com/techinnoveta/java8/lambda/LambdaExp.java
@@ -0,0 +1,8 @@
+package com.techinnoveta.java8.lambda;
+
+public class LambdaExp {
+	   
+    public static void main(String[] args) {
+        
+    }
+}
